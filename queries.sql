a1022d045e4d3c08def1dd9b96be7e52f9d41d9b
luccamicale <luccamicale2015@gmail.com>
2022-12-12 20:17:20 -0300

    Complete data

--- queries.sql
+++ queries.sql
@@ -1,3 +1,6 @@
-/*Queries that provide answers to the questions from all projects.*/
+/* Populate database with sample data. */
 
-SELECT * from animals WHERE name = 'Luna';
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', 'February 3, 2020', 0, true, 10.23);
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', 'November 15, 2018', 2, true, 8);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', 'January 7, 2021', 1, false, 15.04);
+INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', 'May 12, 2017', 5, true, 11);
