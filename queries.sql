0172f89a506933aea90cf5bdec237c27c4b26d98
luccamicale <luccamicale2015@gmail.com>
2022-12-13 09:09:07 -0300

    change de info

--- queries.sql
+++ queries.sql
@@ -1,6 +1,18 @@
-/* Populate database with sample data. */
+/*Queries that provide answers to the questions from all projects.*/
 
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', 'February 3, 2020', 0, true, 10.23);
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Gabumon', 'November 15, 2018', 2, true, 8);
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Pikachu', 'January 7, 2021', 1, false, 15.04);
-INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Devimon', 'May 12, 2017', 5, true, 11);
+/* Find all animals whose name ends in "mon". */
+SELECT * FROM animals WHERE NAME LIKE '%mon';
+/* List the name of all animals born between 2016 and 2019. */
+SELECT * FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-31';
+/*List the name of all animals that are neutered and have less than 3 escape attempts.*/
+SELECT * FROM animals WHERE neutered='t' AND escape_attempts < 3;
+/*List the date of birth of all animals named either "Agumon" or "Pikachu".*/
+SELECT * FROM animals WHERE name='Agumon' OR name='Pikachu';
+/*List name and escape attempts of animals that weigh more than 10.5kg*/
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
+/*Find all animals that are neutered.*/
+SELECT * FROM animals WHERE neutered='t';
+/*Find all animals not named Gabumon.*/
+SELECT * FROM animals WHERE name!='Gabumon';
+/*Find all animals with a weight between 10.4kg and 17.3kg (including the animals with the weights that equals precisely 10.4kg or 17.3kg)*/
+SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
