4d869e91afc388254821048335580d598c833e73
luccamicale <luccamicale2015@gmail.com>
2022-12-14 18:44:27 -0300

    ADD SCHEMA AND ALTER

--- schema.sql
+++ schema.sql
@@ -9,3 +9,28 @@ CREATE TABLE animals (
     weight_kg DECIMAL,
     species VARCHAR(100)
 );
+
+CREATE TABLE owners (
+    id INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+    full_name VARCHAR,
+    age INTEGER
+);
+
+CREATE TABLE species (
+    id INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+    name VARCHAR
+);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD CONSTRAINT species_id
+FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals
+ADD owner_id INTEGER;
+
+ALTER TABLE animals
+ADD CONSTRAINT owner_id
+FOREIGN KEY (owner_id) REFERENCES owners(id);
