3943707f2989027a132cc3f350d47b9dda649b79
amare1990 <amaremek@gmail.com>
2022-12-20 00:25:03 +0300

    Create INDEX for three queries

--- schema.sql
+++ schema.sql
@@ -51,3 +51,7 @@ CREATE TABLE visits(
 
 -- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
+
+CREATE INDEX animal_id_index on visits(animal_id);
+CREATE INDEX vet_id_index on visits(vet_id); 
+CREATE INDEX email_Index on owners(email); 
