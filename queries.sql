112f94c66492ea4c81650c689590d223c0f31888
amare1990 <amaremek@gmail.com>
2022-12-20 00:04:25 +0300

    Check performance using Explain ANALYZE command

--- queries.sql
+++ queries.sql
@@ -197,3 +197,9 @@ GROUP BY
 species.name
 ORDER BY COUNT(animals.species_id) DESC
 LIMIT 1;
+
+
+/*   Block#2    */
+EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
+EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
