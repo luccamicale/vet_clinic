2032e4c4ec927a9807e586d4122facab3a767f1c
amare1990 <amaremek@gmail.com>
2022-12-20 22:46:30 +0300

    Create invoices table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -8,3 +8,9 @@ CREATE TABLE patients (
 CREATE TABLE medical_histories(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
 admitted_at time, patient_id INT, 
 CONSTRAINT patient_hist FOREIGN KEY (patient_id) REFERENCES patients(id), status VARCHAR(50));
+
+CREATE TABLE invoices (id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+total_amount decimal, generated_at time, payed_at time, 
+medical_history_id INT, CONSTRAINT invoice_hist FOREIGN KEY (medical_history_id) 
+REFERENCES medical_histories (id));
+
