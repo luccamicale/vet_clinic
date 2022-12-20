380bb3f51b3ab63fa7f53f87523accbabac4da2d
amare1990 <amaremek@gmail.com>
2022-12-20 22:34:34 +0300

    Create medical_histories table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -4,3 +4,7 @@ CREATE TABLE patients (
   date_of_birth DATE,
   PRIMARY KEY(id)
 );
+
+CREATE TABLE medical_histories(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
+admitted_at time, patient_id INT, 
+CONSTRAINT patient_hist FOREIGN KEY (patient_id) REFERENCES patients(id), status VARCHAR(50));
